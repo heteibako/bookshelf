882abf1eb8ac0142cbf46b1d59b0b1b35763d968
Szabi Hetei-Bako <hetei_bako@yahoo.com>
2020-07-01 09:44:06 +0200

    navbar refactor

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { UncontrolledCollapse, NavbarBrand, Navbar, NavItem, Nav, Container } from 'reactstrap';
+import { UncontrolledCollapse, Navbar, Nav, Container } from 'reactstrap';
 export default function NavBar() {
   return (
     <Navbar className="bg-primary" expand="lg">
       <Container>
         <div className="navbar-translate">
-          <NavbarBrand href="#pablo" onClick={(e) => e.preventDefault()}>
+          <Link className="navbar-brand" to="/">
             My Reads
-          </NavbarBrand>
+          </Link>
           <button className="navbar-toggler" id="example-navbar-primary" type="button">
             <span className="navbar-toggler-bar bar1"></span>
             <span className="navbar-toggler-bar bar2"></span>
